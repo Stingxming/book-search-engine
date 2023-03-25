6ad617dc9ef4a8eda8d7ba4bb2896b17991a2c27
Mathew Chandler <Cascabelrock@gmail.com>
2023-03-24 18:08:16 -0700

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://cascabelrock:Nazikill4r!@cluster0.ug688m5.mongodb.net/bookData?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
