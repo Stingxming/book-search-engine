e8a2618c7422302663d2e5edd68aa8b0184d609c
Mathew Chandler <Cascabelrock@gmail.com>
2023-03-24 17:22:14 -0700

    deploy please

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://cascabelrock:Nazikill4r!@cluster0.ug688m5.mongodb.net/book-search-buddy?retryWrites=true&w=majority",
+    "mongodb+srv://cascabelrock:Nazikill4r!@cluster0.ug688m5.mongodb.net/bookData?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
